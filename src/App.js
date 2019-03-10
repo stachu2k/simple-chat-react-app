1e2085552c88501828a2d2fce9b2463bb73504f3
Paweł Staś <pawelstas@wp.pl>
2019-03-10 14:27:00 +0100

    Code changes to App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
+import './styles.scss';
+import Message from 'components/Message/Message';
+import SelectRoom from 'components/SelectRoom/SelectRoom';
+import Input from 'components/Input/Input';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={styles.App}>
+        <div className={styles.Room}>
+          <SelectRoom/>
+        </div>
+        <div className={styles.Text}>
+          <Message/>
+          <Message/>
+          <Message/>
+        </div>
+        <div className={styles.input}>
+          <Input/>
+        </div>
       </div>
     );
   }
