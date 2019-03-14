0417b6a3f9360656172fb68c6334873997393ffd
Paweł Staś <pawelstas@wp.pl>
2019-03-14 12:59:48 +0100

    Exported components to index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Input } from 'components/Input/Input';
+export { default as InputWrapper } from 'components/InputWrapper/InputWrapper';
+export { default as MessageArea } from 'components/MessageArea/MessageArea';
+export { default as Message } from 'components/Message/Message';
+export { default as SelectRoom } from 'components/SelectRoom/SelectRoom';
+export { default as SelectRoomWrapper } from 'components/SelectRoomWrapper/SelectRoomWrapper';
