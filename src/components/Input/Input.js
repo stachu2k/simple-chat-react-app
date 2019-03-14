1c382c2daafb0eade6a223e9ef10734ba14a2330
Paweł Staś <pawelstas@wp.pl>
2019-03-14 13:08:15 +0100

    Changed Send button to input element with submit type. Added ref to text input. Added form element with onSubmit event handler.

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 
 class Input extends Component {
+  _input = undefined;
+
+  _submitForm = e => {
+    e.preventDefault();
+
+    if (!this._input.value.trim()) {
+      return;
+    }
+
+    this.props.sendMessage(
+      this._input.value,
+      this.props.nickname,
+      new Date().toDateString(),
+      1,
+    );
+
+    this._input.value = '';
+  }
+
   render() {
     return (
-      <div className="field has-addons">
-        <div className="control is-expanded">
-          <input className="input" type="text" placeholder="Message" />
+      <form onSubmit={e => this._submitForm(e)}>
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <input
+              className="input"
+              type="text"
+              placeholder="write message"
+              ref={el => this._input = el}
+            />
+          </div>
+          <div className="control">
+            <input className="button" type="submit" value="Send"/>
+          </div>
         </div>
-        <div className="control">
-          <a className="button" href="/">Send</a>
-        </div>
-      </div>
+      </form>
     );
   }
 }
