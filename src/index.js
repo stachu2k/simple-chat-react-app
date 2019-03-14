cd3a667fc5476c676ea80faf1500d55d4a687fc0
Paweł Staś <pawelstas@wp.pl>
2019-03-14 13:08:48 +0100

    Added store and Provider component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { changeNickname } from 'actions';
+import chatApp from 'reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(chatApp);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//--------------------- TEST ------------------------------------------------------
+console.log(store.getState());
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+store.dispatch(changeNickname('Stachu'));
+unsubscribe();
+window.store = store;
+//--------------------- /TEST ------------------------------------------------------
