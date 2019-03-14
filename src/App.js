f7ee5c8aa07ac2e63f900e20e3f692685b84ef05
Paweł Staś <pawelstas@wp.pl>
2019-03-14 13:01:59 +0100

    Added wrappers and containers and removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
+import { InputWrapper, SelectRoomWrapper } from 'components';
+import { MessageAreaCont } from 'containers';
 import styles from './App.module.scss';
 import './styles.scss';
-import Message from 'components/Message/Message';
-import SelectRoom from 'components/SelectRoom/SelectRoom';
-import Input from 'components/Input/Input';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <div className={styles.Room}>
-          <SelectRoom/>
-        </div>
-        <div className={styles.Text}>
-          <Message/>
-          <Message/>
-          <Message/>
-        </div>
-        <div className={styles.input}>
-          <Input/>
-        </div>
+      <div className={styles['app']}>
+        <SelectRoomWrapper/>
+        <MessageAreaCont/>
+        <InputWrapper/>
       </div>
     );
   }
