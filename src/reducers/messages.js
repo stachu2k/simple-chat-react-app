1c5b488fbd9f9b4aac4a2c6150797c72b1a85e33
Paweł Staś <pawelstas@wp.pl>
2019-03-29 12:23:19 +0100

    Modified messages reducer

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,6 +1,15 @@
 import { combineReducers } from 'redux';
 import types from 'actions/actionTypes';
 
+function error(state = '', action) {
+  switch (action.type) {
+    case types.FETCH_MESSAGES_FAILURE:
+      return action.error;
+    default:
+     return state;
+  }
+}
+
 function isFetching(state = false, action) {
   switch (action.type) {
     case types.FETCH_MESSAGES_REQUEST:
@@ -16,23 +25,33 @@ function items(state = [], action) {
   switch (action.type) {
     case types.FETCH_MESSAGES_SUCCESS:
       return action.items;
-    case types.SEND_MESSAGE:
+    case types.SEND_MESSAGE_REQUEST:
       return [
-          ...state,
-          {
-            id: state.length + 1,
-            text: action.text,
-            author: action.author,
-            created: action.created,
-            room: action.room,
-          }
-        ]
+        ...state,
+        {
+          id: action.message.id,
+          text: action.message.text,
+          author: action.message.author,
+          created: action.message.created,
+          room: action.message.room,
+          error: action.error,
+        }
+      ];
+    case types.SEND_MESSAGE_FAILURE:
+      return state.map(message => (
+        message.id === action.tempId ? {...message, error: action.error} : message
+      ));
+    case types.SEND_MESSAGE_SUCCESS:
+      return state.map(message => (
+        message.id === action.tempId ? action.message : message
+      ));
     default:
       return state;
   }
 }
 
 const messages = combineReducers({
+  error,
   isFetching,
   items,
 })
