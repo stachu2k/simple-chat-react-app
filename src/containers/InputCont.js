436ad9f7a1dae8efcb8273f0db689293a6d1521e
Paweł Staś <pawelstas@wp.pl>
2019-03-29 12:21:00 +0100

    Added selectedRoom to props and changed import for sendMessage

--- src/containers/InputCont.js
+++ src/containers/InputCont.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Input from 'components/Input/Input';
-import { sendMessage } from 'actions';
+import { sendMessage } from 'operations';
 
 const mapStateToProps = state => ({
   nickname: state.nickname,
+  selectedRoom: state.selectedRoom,
 });
 
 const mapDispatchToProps = dispatch => (
