4b62e785d830acec1f36c44ca9abb9f214b5c97b
Paweł Staś <pawelstas@wp.pl>
2019-03-29 12:19:16 +0100

    Added conversion from UTC to Local date

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { convertUTCDateToLocalDate } from 'lib';
 import styles from './Message.module.scss';
 
 class Message extends Component {
   render() {
     const { author, created, text } = this.props;
+    const localizedDate = convertUTCDateToLocalDate(new Date(created * 1000));
     return (
       <div className={styles['message']}>
         <div>
-          <strong>{author}</strong> <small>{new Date(created * 1000).toLocaleString()}</small>
+          <strong>{author}</strong> <small>{localizedDate.toLocaleString()}</small>
         </div>
         <div>
           {text}
